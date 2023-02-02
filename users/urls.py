42e7a6d13455d2bfec48135b3fef766445ca4bd2
Ritchie Programmer <officialforrichardk@gmail.com>
2023-02-02 11:56:58 +0300

    add log in and signup functionality

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('login/',views.login_view, name='login'),
+    path('signup/',views.signup_view, name='signup')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm,SignUpForm
+
 
 def login_view(request):
     if request.method=='POST':
@@ -15,5 +16,21 @@ def login_view(request):
     else:
         form=LoginForm()
     return render(request, 'users/login.html',{'form':form})
+
+def signup_view(request):
+    if request.method=='POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            raw_password=form.cleaned_data.get('password1')
+            user=authenticate(username=username,password=raw_password)
+            login(request,user)
+            return redirect('home')
+    else:
+        form=SignUpForm()
+    return render(request, 'users/signup.html',{'form':form})
+
+
 
 
